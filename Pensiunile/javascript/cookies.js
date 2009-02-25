6c6fb64507437cd1756c75d2338361922c97bb8c
ioan.szilagyi <ioan.szilagyi@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-02-25 15:46:09 +0000

    cateva modificari minore de sintaxa

--- Pensiunile/javascript/cookies.js
+++ Pensiunile/javascript/cookies.js
@@ -2,7 +2,7 @@
 
 function getCookie(c_name){
 	if (document.cookie.length>0){
-		c_start=document.cookie.indexOf(c_name + "=");
+		c_start = Document.cookie.indexOf(c_name + "=");
 		if (c_start!=-1){
 			c_start=c_start + c_name.length+1;
 			c_end=document.cookie.indexOf(";",c_start);
@@ -54,14 +54,14 @@ function addLoadListener(fn) {
 	
 	if(typeof window.addEventListener != null) {
 		
-		window.addEventListener('load', fn, false);
+		Window.addEventListener('load', fn, false);
 		
 	} else if(typeof document.addEventListener != undefined) {
 		
-		document.addEventListener('load', fn, false);
+		Document.addEventListener('load', fn, false);
 	} else if (typeof window.attachEvent != undefined){
 		
-		window.attachEvent('load', fn);
+		Window.attachEvent('load', fn);
 		
 	} else {
 		var oldfn = window.onload;
