32b9947006db60eefcd9cb0250a8b0976a079071
ioan.szilagyi <ioan.szilagyi@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-03-09 09:41:39 +0000

    am creat folderul vo(value objects) in care am creat clasa pensiune.php

--- trunk/Pensiunile/vo/pensiune.php
+++ trunk/Pensiunile/vo/pensiune.php
@@ -1,6 +1,23 @@
 <?php
-class pensiune {
+class Pensiune {
+	
 	public $id;
+	public $main_photo;
+	public $name;
+	public $categ;
+	public $zt;
+	public $jud;
+	public $loc;
+	public $tel;
+	public $mail;
+	public $web;
+	public $pret;
+	public $disp;
+	
+	public function __construct(){
+		
+		
+	}
 	
 }
 
