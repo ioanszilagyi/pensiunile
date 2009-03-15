cfdcbc63c2f982d72e732a43347b4082bd03168d
ioan.szilagyi <ioan.szilagyi@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-03-15 12:59:22 +0000

    schimbarea structurii clasei pensiunie

--- trunk/Pensiunile/vo/pensiune.php
+++ trunk/Pensiunile/vo/pensiune.php
@@ -1,18 +1,26 @@
 <?php
 class Pensiune {
 	
-	public $id;
-	public $main_photo;
-	public $name;
-	public $categ;
-	public $zt;
-	public $jud;
-	public $loc;
-	public $tel;
-	public $mail;
-	public $web;
-	public $pret;
-	public $disp;
+	public $id = 0;
+	public $name = "Nume implicit";
+	public $photo = "/photos/no-image.png";
+	
+	public $description = "Aceasta este o descriere scurta.";
+	public $category = "2";
+	
+	public $address = "Str. Principala nr. 1";
+	
+	//public $contry = "Romania";
+	public $jud = "Bihor";
+	public $loc = "Oradea";
+	public $zona_turistica = "Ardeal";
+	
+	public $phone = "+40 720 123456";
+	public $email = "nume@pensiunile.ro";
+	public $web = "www.pensiunile.ro";
+	
+	public $ratings = "70%(150 votes)"; // se va calula in procente %
+	public $price = "50 - 150 EURO"; // se va da plaja de preturi
 	
 	public function __construct(){
 		
