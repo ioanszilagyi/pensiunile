828bf4f6f5f0d5c2123886c813c94a66835d02ab
ioan.szilagyi <ioan.szilagyi@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-06-19 23:16:51 +0000

    am continuat cu functia care selecteaza pensiunile din baza de date ca sa tina cont de selectiile filtrului

--- trunk/Pensiunile/includes/functions.php
+++ trunk/Pensiunile/includes/functions.php
@@ -150,13 +150,55 @@ return $row_set;
 
 //get pensiune where....
 
-function get_pensiuni($search_field, $selected_zona, $selected_judet, $selected_categorie){
+function get_pensiuni($search_field, $selected_zona = "0", $selected_judet = "0", $selected_statiune = "0", $selected_categorie = "0"){
 global $connection;
-//$query = 'SELECT * FROM `cnt_pensiuni` WHERE nume LIKE';// LIMIT 0, 10 ';
-$query = "SELECT * FROM `cnt_pensiuni` WHERE `name` LIKE '%{$search_field}%' "; 
-$row_set = mysql_query($query, $connection);
-confirm_query($row_set);
-return $row_set;
+	 $query = "SELECT
+			`cnt_pensiuni`.`id` AS id,
+			`cnt_pensiuni`.`name` AS name,
+			`cnt_photos`.`file` AS photo_file,
+			`cnt_photos`.`title` AS photo_title,
+			`cnt_pensiuni`.`category` AS category,
+			`cnt_pensiuni`.`description` AS description,
+			`cnt_pensiuni`.`prices` AS prices,
+			`cnt_pensiuni`.`address` AS address,
+			`cnt_pensiuni`.`phone` AS phone,
+			`cnt_pensiuni`.`email` AS email,
+			`cnt_localitati`.`name` AS localitate,
+			`cnt_judete`.`name` AS judet,
+			`cnt_zone_turistice`.`name` AS zona_turistica,			
+			
+			`cnt_pensiuni`.`web` AS web
+			
+			FROM `cnt_pensiuni`
+			
+			LEFT JOIN `cnt_pensiuni_detalii` ON `cnt_pensiuni`.`id` = `cnt_pensiuni_detalii`.`id`
+			LEFT JOIN `cnt_photos` ON `cnt_pensiuni`.`photo_id` = `cnt_photos`.`id`
+			LEFT JOIN `cnt_users` ON `cnt_pensiuni`.`user_id` = `cnt_users`.`id`
+			
+			LEFT JOIN `cnt_localitati` ON `cnt_pensiuni`.`localitate_id` = `cnt_localitati`.`id`
+			LEFT JOIN `cnt_judete` ON `cnt_localitati`.`judet_id` = `cnt_judete`.`id`			
+			
+			LEFT JOIN `cnt_statiuni` ON `cnt_pensiuni_detalii`.`statiune_id` = `cnt_statiuni`.`id`
+			LEFT JOIN `cnt_zone_turistice` ON `cnt_pensiuni_detalii`.`zona_turistica_id` = `cnt_zone_turistice`.`id`
+
+			WHERE `cnt_pensiuni`.`name` LIKE '%{$search_field}%'";
+			
+			if ( $selected_zona != "0") {
+				$query .= " AND `cnt_zone_turistice`.`id`='{$selected_zona}'"; 
+			};
+			if ( $selected_judet != "0") {
+				$query .= " AND `cnt_judete`.`id`='{$selected_judet}'"; 
+			};
+			if ( $selected_statiune != "0") {
+				$query .= " AND `cnt_statiuni`.`id`='{$selected_statiune}'"; 
+			};
+			if ($selected_categorie != "0") {
+				$query .= " AND `cnt_pensiuni`.`category`='{$selected_categorie}'"; 
+			};
+			
+	$row_set = mysql_query($query, $connection);
+	confirm_query($row_set);
+	return $row_set;
 }
 
 function get_pensiuni_name($name) {
@@ -187,7 +229,7 @@ function get_pensiuni_name($name) {
 
 			LEFT JOIN `cnt_users` ON `cnt_pensiuni`.`user_id` = `cnt_users`.`id`
 			
-			WHERE `cnt_pensiuni`.`name` LIKE '%{$name}%' ";
+			WHERE `cnt_pensiuni`.`name` LIKE '%{$name}%'";
 
 	$row_set = mysql_query($query, $connection);
 	confirm_query($row_set);
