ac24a79d876f1a9dde9a252497eab8404735810e
ioan.szilagyi <ioan.szilagyi@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-08-01 10:54:55 +0000

    creat clasa user

---
+++ Pensiunile/vo/user.php
@@ -0,0 +1,19 @@
+<?php
+class User {
+
+	public $id = 0;
+	public $email = "email@email.com";
+	public $password = "password";
+
+	public $name = "Anonim";
+	public $phone = "+40 123 123456";
+        
+	public function __construct(){
+
+
+	}
+
+}
+
+
+?>
