f90062282a5993fa7e1d9475a3f73653c1617a1f
ioan.szilagyi <ioan.szilagyi@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-08-03 12:28:54 +0000

    clasa user

--- trunk/Pensiunile/vo/user.php
+++ trunk/Pensiunile/vo/user.php
@@ -2,10 +2,10 @@
 class User {
 
 	public $id = 0;
-	public $email = "email@email.com";
+	public $email = "contact@pensiunile.ro";
 	public $password = "password";
 
-	public $name = "Anonim";
+	public $name = "Pensiunile";
 	public $phone = "+40 123 123456";
         
 	public function __construct(){
