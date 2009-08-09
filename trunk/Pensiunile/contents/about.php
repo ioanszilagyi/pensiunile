1f98c521235c2fd831a6565c209b96e2a1e63eb2
ioan.szilagyi <ioan.szilagyi@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-08-09 09:34:58 +0000

    atribuirea titlului paginii se va face din index.php, am sters smary->assign din paginile din content

--- trunk/Pensiunile/contents/about.php
+++ trunk/Pensiunile/contents/about.php
@@ -11,8 +11,6 @@ $smarty = $GLOBALS['smarty'];
 
 //--------------------------------------------------------------------
 
-$titlu_pag = "Pensiunile din Romania - Page Under Construction";
-
 //setarea fisierelor css si javascript
 $jsfiles = array('script.js');//, 'javascript/cookies.js');
 $cssfiles = array('master.css');
@@ -20,8 +18,6 @@ $cssfiles = array('master.css');
 //--------------------------------------------------------------------
 //setare variabile din template
 //assign titlul pagini
-
-$smarty->assign('titlu_pag', $titlu_pag);
 
 $smarty->assign('jsfiles', $jsfiles);
 $smarty->assign('cssfiles', $cssfiles);
