e6be0281321736f5a3e0d28cc8685046ed38b4ae
popa.daniel <popa.daniel@81624c92-c791-11dd-a2ab-b939405fd53f>
2009-09-19 20:41:11 +0000

    codul pentru extragerea informatiilor din baza de date in vederea afisaraii lor in pagina pentru delaiile unei pensiuni

---
+++ Pensiunile/contents/show_hostel.php
@@ -0,0 +1,71 @@
+<?php
+//--------------------------------------------------------------------
+//variabilele laute si setate in index.php
+
+//echo "limba e: ".$GLOBALS['lang'];
+$lang = $GLOBALS['lang'];
+$file_name = $GLOBALS['file_name'];
+$smarty = $GLOBALS['smarty'];
+
+//$user_current = unserialize($_SESSION['user_current']);
+
+//--------------------------------------------------------------------
+
+$titlu_pag = "Pensiunile din Romania";
+
+//setarea fisierelor css si javascript
+$jsfiles = array('script.js','home.js');//, 'javascript/cookies.js');
+$cssfiles = array('master.css', 'home.css');
+
+//--------------------------------------------------------------------
+//setare variabile din template
+//assign titlul pagini
+
+$smarty->assign('titlu_pag', $titlu_pag);
+
+$smarty->assign('jsfiles', $jsfiles);
+$smarty->assign('cssfiles', $cssfiles);
+
+//------------------------------------------------------------------
+
+//get pensiune
+$row_set = get_pensiune($_GET['hostel']);
+$i=1;
+
+while($hostel = mysql_fetch_array($row_set)){
+	$hostel_id[$i] = $hostel['id'];
+	$hostel_name[$i] = $hostel['name'];
+	$hostel_photo_file[$i] = $hostel['photo_file'];
+	$hostel_category[$i] = $hostel['category'];
+	$hostel_address[$i] = $hostel['address'];
+	$hostel_localitate[$i] = $hostel['localitate'];
+	$hostel_judet[$i] = $hostel['judet'];
+	$hostel_phone[$i] = $hostel['phone'];
+	$hostel_email[$i] = $hostel['email'];
+	$hostel_web[$i] = $hostel['web'];
+	$hostel_zip[$i] = $hostel['zip'];
+	$hostel_price_min[$i] = $hostel['price_min'];
+	$hostel_price_max[$i] = $hostel['price_max'];
+	$i++;
+}
+
+$smarty->assign('id',$hostel_id[1]);
+$smarty->assign('name',$hostel_name[1]);
+$smarty->assign('photo_file',$hostel_photo_file[1]);
+$smarty->assign('category',$hostel_category[1]);
+$smarty->assign('address',$hostel_address[1]);
+$smarty->assign('localitate',$hostel_localitate[1]);
+$smarty->assign('judet',$hostel_judet[1]);
+$smarty->assign('phone',$hostel_phone[1]);
+$smarty->assign('email',$hostel_email[1]);
+$smarty->assign('web',$hostel_web[1]);
+$smarty->assign('zip',$hostel_zip[1]);
+$smarty->assign('price_min',$hostel_price_min[1]);
+$smarty->assign('price_max',$hostel_price_max[1]);
+//---------------------
+
+//afiseaza pagina (.TPL)
+//echo $file_name."_".$lang;
+smarty_display($file_name, $lang, $smarty);
+
+?>
